70949203f8a7cf548f6f227fa48b06c29d8b1d57
lisajamhoury <lisajamhoury@gmail.com>
2016-08-02 18:01:51 -0400

    Reorganize folders and skeleton working in p5

--- p5remote/p5remoteexamples.js
+++ p5remote/p5remoteexamples.js
@@ -12,7 +12,7 @@ var image = null;
 
 //variables for skeleton
 var colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff'];
-var HANDSIZE = 20;
+var HANDSIZE = 40;
 var HANDCLOSEDCOLOR = "red";
 var HANDOPENCOLOR = "green";
 var HANDLASSOCOLOR = "blue"; 
@@ -59,7 +59,8 @@ function makeConnection() {
       case 'clearCanvas':
         console.log('Clear Canvas');
         image.src = " ";
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        //context.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+        clear();
       break;
       
       case 'bodyFrame':
@@ -76,41 +77,56 @@ function makeConnection() {
 }
 
 window.addEventListener('load', function() {
-  canvas = document.getElementById('outputCanvas');
-  context = canvas.getContext('2d');
+  //canvas = document.getElementById('outputCanvas');
+  //context = canvas.getContext('2d');
 
-  imageData = context.createImageData(canvas.width, canvas.height);
-  imageDataSize = imageData.data.length;
-  imageDataArray = imageData.data;
+  // imageData = context.createImageData(canvas.width, canvas.height);
+  // imageDataSize = imageData.data.length;
+  // imageDataArray = imageData.data;
 
-  image = document.getElementById('image');
+   //image = document.getElementById('image');
 });
+
+// p5 setup
+function setup() {
+  canvas = createCanvas(512, 424);
+  canvas.parent("container");
+  console.log(canvas.canvas.width, canvas.canvas.height);
+
+  context = canvas.drawingContext;
+
+  image = createImage();
+
+  noStroke();
+}
+
 
 function setImageSize(size) {
   if (size == 'color') {
-    console.log('resetting large');
-    canvas.width = 960;
-    canvas.height = 540;
+    console.log('resetting color');
+    canvas.canvas.width = 960;
+    canvas.canvas.height = 540;
     image.width = 960;
     image.height = 540;
   } else if (size == 'depth') {
-    console.log('resetting small');
-    canvas.width = 1024;
-    canvas.height = 848;
-    image.width = 1024;
-    image.height = 848;
+    console.log('resetting depth');
+    canvas.canvas.width = 512;
+    canvas.canvas.height = 424;
+    image.width = 512;
+    image.height = 424;
   }
 }
 
 function showHeight(data) {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  //context.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+  clear();
   context.beginPath();
   context.fillStyle = "red";
-  context.arc(data.joint.colorX * canvas.width, data.joint.colorY * canvas.height, 10, 0, Math.PI * 2, true);
+  context.arc(data.joint.colorX * canvas.canvas.width, data.joint.colorY * canvas.canvas.height, 10, 0, Math.PI * 2, true);
   context.fill();
   context.closePath();
   context.font = "48px sans";
-  context.fillText(data.distance.toFixed(2) + "m", 20 + data.joint.colorX * canvas.width, data.joint.colorY * canvas.height);
+  context.fillText(data.distance.toFixed(2) + "m", 20 + data.joint.colorX * canvas.canvas.width, data.joint.colorY * canvas.canvas.height);
 }
 
 
@@ -134,27 +150,22 @@ function updateHandState(handState, jointPoint) {
 }
 
 function drawHand(jointPoint, handColor) {
-  // draw semi transparent hand cicles
+  // draw hand cicles
   var handData = {depthX: jointPoint.depthX, depthY: jointPoint.depthY, handColor: handColor, handSize: HANDSIZE};
-  context.globalAlpha = 0.75;
-  context.beginPath();
-  context.fillStyle = handColor;
-  context.arc(jointPoint.depthX * canvas.width, jointPoint.depthY * canvas.height, HANDSIZE, 0, Math.PI * 2, true);
-  context.fill();
-  context.closePath();
-  context.globalAlpha = 1;
+  fill(handColor);
+  ellipse(jointPoint.depthX * canvas.canvas.width, jointPoint.depthY * canvas.canvas.height, HANDSIZE, HANDSIZE);
 }
 
 
 function bodyTracked(body) {
   // clear canvas each time
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  clear();
+  // draw body joints
   for(var jointType in body.joints) {
     var joint = body.joints[jointType];
-    context.fillStyle = colors[index];
-    context.fillRect(joint.depthX * canvas.width, joint.depthY * canvas.height, 10, 10);
+    fill(colors[index]);
+    rect(joint.depthX * canvas.canvas.width, joint.depthY * canvas.canvas.height, 10, 10);
     var skeletonJointData = {color: colors[index], depthX: joint.depthX, depthY: joint.depthY};
-
   }
   // draw hand states
   // 7 is left hand in Kinect2
