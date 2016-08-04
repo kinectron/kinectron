8536b0f9f4df8e9e932fa55d0b75104771e5b142
lisajamhoury <lisajamhoury@gmail.com>
2016-08-04 11:32:01 -0400

    Remove unused imageData variables

--- remoteexamples.js
+++ remoteexamples.js
@@ -4,9 +4,6 @@ var connection = null;
 
 var canvas = null;
 var context = null;
-var imageData = null;
-var imageDataSize = null;
-var imageDataArray = null;
 
 var image = null;
 
@@ -78,10 +75,6 @@ function makeConnection() {
 window.addEventListener('load', function() {
   canvas = document.getElementById('outputCanvas');
   context = canvas.getContext('2d');
-
-  imageData = context.createImageData(canvas.width, canvas.height);
-  imageDataSize = imageData.data.length;
-  imageDataArray = imageData.data;
 
   image = document.getElementById('image');
 });
