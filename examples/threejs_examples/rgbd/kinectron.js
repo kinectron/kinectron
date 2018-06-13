781e3a4e91a0bf3169aed017cb6bdafedde8b04e
lisajamhoury <lisajamhoury@gmail.com>
2018-06-13 13:32:36 -0400

    Add three kinectron example

--- examples/threejs_examples/rgbd/kinectron.js
+++ examples/threejs_examples/rgbd/kinectron.js
@@ -10,7 +10,7 @@ var busy = false;
 function initKinectron() {
 
 	// Define and create an instance of kinectron
-  var kinectronIpAddress = ""; // FILL IN YOUR KINECTRON IP ADDRESS HERE
+  var kinectronIpAddress = "172.16.222.192"; // FILL IN YOUR KINECTRON IP ADDRESS HERE
   kinectron = new Kinectron(kinectronIpAddress);
 
   // Open connection to Kinectron app
