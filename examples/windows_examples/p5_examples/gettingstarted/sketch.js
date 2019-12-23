e3029d8671cd79896eb189b7ee2a337fdc3c07bb
montoyamoraga <montoyamoraga@gmail.com>
2019-12-23 11:19:36 -0500

    add new gettingstarted example

--- examples/windows_examples/p5_examples/gettingstarted/sketch.js
+++ examples/windows_examples/p5_examples/gettingstarted/sketch.js
@@ -9,19 +9,40 @@ Kinect Windows camera feeds example using p5.js
 === */
 //
 
-// Declare kinectron
+// declare variable for kinectron
 let kinectron = null;
-let frameP;
 
+// FILL IN YOUR kinectron server ip address here
+// ip address is a string between four 
+// this is a string made up from four numbers
+// each number is between 0 and 255 and separated with periods
+let kinectronServerIPAddress = "18.121.129.122";
+
+// declare new HTML elements for displaying text
+let textKinectronServerIP;
+let textCurrentFeed;
+let textFramerate;
+
+// variable for storing current 
+let currentFeed = "none";
+
+// setup() is a p5.js function
+// setup() runs once, at the beginning
 function setup() {
+
+  // create canvas 500px wide x 500px high
   createCanvas(500, 500);
-  background(0);
 
-  frameP = createP("");
+  // white background
+  background(255);
 
-  // Define and create an instance of kinectron
-  let kinectronIpAddress = ""; // FILL IN YOUR KINECTRON IP ADDRESS HERE
-  kinectron = new Kinectron(kinectronIpAddress);
+  // create new HTML <p> elements for displaying text
+  textKinectronServerIP = createP("");
+  textCurrentFeed = createP("");
+  textFramerate = createP("");
+
+  // create an instance of kinectron
+  kinectron = new Kinectron(kinectronServerIPAddress);
 
   // Set kinect type to windows
   kinectron.setKinectType("windows");
@@ -29,36 +50,53 @@ function setup() {
   // Connect with application over peer
   kinectron.makeConnection();
 
-  // Set callbacks
+  // define callbacks for color, depth and infrared
   kinectron.setColorCallback(drawFeed);
   kinectron.setDepthCallback(drawFeed);
   kinectron.setInfraredCallback(drawFeed);
 }
 
+// draw() is a p5.js function
+// after setup() runs once, draw() runs on a loop
 function draw() {
-  let fps = frameRate();
+
+  // p5.js drawing settings
+  // black fill() and stroke()
   fill(0);
   stroke(0);
-  frameP.html(fps.toFixed(0));
+
+  // update text with current parameters
+  textKinectronServerIP.html("Kinectron Server IP address: " + kinectronServerIPAddress);
+  textCurrentFeed.html("current feed: " + currentFeed);
+  textFramerate.html("fps: " + frameRate().toFixed(0));
 }
 
-// Choose camera to start based on key pressed
+// keyPressed() is a p5.js function
+// choose camera to start based on key pressed
 function keyPressed() {
   if (keyCode === ENTER) {
     kinectron.startColor();
+    currentFeed = "color";
   } else if (keyCode === UP_ARROW) {
     kinectron.startDepth();
+    currentFeed = "depth";
   } else if (keyCode === DOWN_ARROW) {
     kinectron.startInfrared();
+    currentFeed = "infrared";
   } else if (keyCode === RIGHT_ARROW) {
     kinectron.stopAll();
+    currentFeed = "none";
   }
 }
 
-function drawFeed(img) {
-  // Draws feed using p5 load and display image functions
-  loadImage(img.src, function(loadedImage) {
+// callback function when feed sends a new frame
+function drawFeed(newFrame) {
+  // loadImage() is a p5.js function
+  // load new frame from feed and then place it on p5.js canvas
+  loadImage(newFrame.src, function(loadedFrame) {
+    // white background
     background(255);
-    image(loadedImage, 0, 0);
+    // place the frame from kinectron at (0,0)
+    image(loadedFrame, 0, 0);
   });
 }
